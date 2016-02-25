50cb35d38fe94ea372ee57b90639f2ee50000305
200246900 <200246900@student.georgianc.on.ca>
2016-02-25 13:39:12 -0500

    Added Landing, People, Contact Pages)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index' });
 });
-
+/* GET Landing page. */
+router.get('/index', function (req, res, next) {
+    res.render('index', { title: 'Landing' });
+});
+/* GET people page. */
+router.get('/people', function (req, res, next) {
+    res.render('people', { title: 'People' });
+});
+/* GET contact page. */
+router.get('/contact', function (req, res, next) {
+    res.render('contact', { title: 'Contact' });
+});
 module.exports = router;
